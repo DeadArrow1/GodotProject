03b05e12cae42373a62ff90bb7f90c8ac96392fb
PanSmS <danektomik@gmail.com>
2024-07-09 00:09:10 +0200

    Talent tree
    Completed talent tree nodes + branching

--- skill_tree.gd
+++ skill_tree.gd
@@ -8,7 +8,13 @@ func _on_btn_close_pressed():
 func _on_btn_confirm_pressed():
 	queue_free()
 
+<<<<<<< Updated upstream
 func _input(ev):
 	if Input.is_key_pressed(KEY_ESCAPE):
 		print("EscapeKeyPressed")
 		get_node(".").set_scale(Vector2(0,0))
+=======
+func _process(delta):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		queue_free()
+>>>>>>> Stashed changes
